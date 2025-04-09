b228961694622337e3c6fa94935b16672b5dd048
root <yingintech@gmail.com  git config --global user.name Ying>
2025-04-09 06:57:47 +0000

    is root

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -1,4 +1,7 @@
 #include "binary_trees.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <stddef.h>
 
 /**
  * binary_tree_is_leaf - Checks if a node is a leaf of a binary tree.
@@ -11,8 +14,6 @@
 int binary_tree_is_leaf(const binary_tree_t *node)
 {
 	if (node == NULL || node->left != NULL || node->right != NULL)
-		return (0);
-	
-	return(1);
-
+		return (0);	
+	return (1);
 }
