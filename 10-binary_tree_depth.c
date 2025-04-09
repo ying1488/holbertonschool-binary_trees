c9a6350227dc7683f211e23271db6d5ff9b8f450
root <yingintech@gmail.com  git config --global user.name Ying>
2025-04-09 13:44:24 +0000

    depth

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -4,8 +4,8 @@
 #include "binary_trees.h"
 
 /**
- *binary_tree_depth - function that measures the
- depth of a node in a binary tree
+ *binary_tree_depth - function that measures the 
+ *depth of a node in a binary tree
  * @tree: pointer to the root node of the tree to measure the height.
  * Return: 0
  */
