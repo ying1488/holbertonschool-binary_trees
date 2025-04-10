1c6c44420691b59281c7d43f10b21fac8b1c2854
root <yingintech@gmail.com  git config --global user.name Ying>
2025-04-10 13:37:38 +0000

    file setup

---
+++ 15-binary_tree_is_full.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "binary_trees.h"
+
+/**
+ * binary_tree_is_full- function that checks if a binary tree is full.
+ * @tree: pointer to the root node of the tree to measure the balance factor.
+ * Return: 0
+ */
+
+int binary_tree_is_full(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return (0);
+
+
+}
