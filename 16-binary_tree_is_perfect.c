07df877c87b84f7e64fd4cb6fce605af727df6f0
root <yingintech@gmail.com  git config --global user.name Ying>
2025-04-11 04:59:04 +0000

    is perfect

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -2,42 +2,40 @@
 #include <stdlib.h>
 #include <string.h>
 #include "binary_trees.h"
-/**
- * binary_tree_is_perfect- function that 
- * checks if a binary tree is perfect
- * @tree:  pointer to the root node of the tree to measure the balance factor
- *
- * Return: 0.
- */
 
 
-
-
- get height 
- get size 
-
-
- size_t total_nodes = (1UL << (height + 1 )) - 1);
-	if totel_nodes == size
-	return ;
-return 0 ; 
-
-/**int binary_tree_is_perfect (const binary_tree_t *tree)
+int binary_tree_is_perfect(const binary_tree_t *tree)
 {
+	int l;
+	int r;
+
 	if (tree == NULL)
+	{
 		return (0);
-
+	}
 	
-	/*If the root is a leaf node, then the tree is a perfect binary tree.*/
-	if (tree->left == NULL && tree->right == NULL)
+	l = binary_tree_is_perfect(tree->left);
+       	r = binary_tree_is_perfect(tree->right);
+
+	if (l == 0 && r == 0)
+	{
 		return (1);
-	/*If the root has only one child, then the tree is not a perfect binary tree*/
-	if (tree->left == NULL || tree->right == NULL)
+	}	
+
+	if (l == r)
+	{
+		if (tree->parent == NULL)
+		{
+			return (1);
+		}
+		else
+		{
+			return (l + 1);
+		}
+
+	}
+	else
+	{
 		return (0);
-	/*Recursively check the left and right subtrees.*/
-	/*If both are perfect binary trees, then the entire tree is a perfect binary tree.*/
-/**	return (binary_tree_is_perfect(tree->left) && binary_tree_is_perfect(tree->right));
-
+	}
 }
-*/
-
