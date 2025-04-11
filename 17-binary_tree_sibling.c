d34deb8d2cb985d5b71f0b2d4d4a8e98789511e1
root <yingintech@gmail.com  git config --global user.name Ying>
2025-04-11 05:21:36 +0000

    is sibling

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -4,8 +4,22 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_height - function that measures the height of a binary tree
- * @tree: pointer to the root node of the tree to measure the height.
+ * binary_tree_sibling - function that finds the sibling
+ * of a node
+ * @node: pointer to the node to find the sibling.
  * Return: 0
  */
 
+binary_tree_t *binary_tree_sibling(binary_tree_t *node)
+{
+	if (node == NULL || node->parent == NULL)
+		return (NULL);
+
+	if (node->parent->left == node)
+		return (node->parent->right);
+
+	if (node->parent->right == node)
+		return (node->parent->left);
+
+	return (NULL);
+}
