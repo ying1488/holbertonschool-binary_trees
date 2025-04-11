9f044e343feccaed178a774ed891d4f06a2df2fd
root <yingintech@gmail.com  git config --global user.name Ying>
2025-04-11 05:08:29 +0000

    fixed betty

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -4,8 +4,8 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_perfect- A function that checks if a binary tree.
- * @tree: Pointer to root node to check 
+ * binary_tree_is_perfect- A function that checks if a binary tree.
+ * @tree: Pointer to root node to check
  * Return: 0
  */
 int binary_tree_is_perfect(const binary_tree_t *tree)
---
+++ 17-binary_tree_sibling.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "binary_trees.h"
+
+/**
+ * binary_tree_height - function that measures the height of a binary tree
+ * @tree: pointer to the root node of the tree to measure the height.
+ * Return: 0
+ */
+
