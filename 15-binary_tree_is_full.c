1bb99ca65e23d7d098056fdbfd0961b2478a347f
root <yingintech@gmail.com  git config --global user.name Ying>
2025-04-11 01:11:08 +0000

    is full

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -17,9 +17,10 @@ int binary_tree_is_full(const binary_tree_t *tree)
 	 /*0 is not full , 1 is full*/
 
 	if (tree->left == NULL && tree->right == NULL)
-		return(1);
-	
+		return (1);
+
 	if (tree->left && tree->right)
-		return (binary_tree_is_full(tree->left) && binary_tree_is_full(tree->right));	
+		return (binary_tree_is_full(tree->left) &&
+			binary_tree_is_full(tree->right));
 	return (0);
 }
